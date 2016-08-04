a2b4bca1a2a441304ee09ccd97690088b56b2ad5
rjorgestudent <rjorge@student.chalmers.se>
2016-08-04 13:16:27 +0200

    Archivos de salida

--- PreProcessor/PreProcessor.m
+++ PreProcessor/PreProcessor.m
@@ -3,11 +3,11 @@ close all
 clc
 
 curr_folder = dir;
-imag_folder = [curr_folder '\Data\Blobs'];
+imag_folder = [pwd '\Comprimidas'];
 totImages   = length(curr_folder(not([curr_folder.isdir])))-2;
 %%
 for image_nbr = 1:totImages
-    image_name = ['avocados_' num2str(image_nbr) '.jpeg'];
+    image_name = ['DSC' num2str(image_nbr) '.jpeg'];
     image_file = imread(image_name);
     data(image_nbr, :, :, :) = image_file;        
 end
