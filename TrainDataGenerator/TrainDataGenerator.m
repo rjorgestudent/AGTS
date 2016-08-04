de8ffbbc4e18f095754fd0f08015d7ccca7e1a77
rjorgestudent <rjorge@student.chalmers.se>
2016-08-04 16:00:47 +0200

    TrainData control de Thumbs file

--- TrainDataGenerator/TrainDataGenerator.m
+++ TrainDataGenerator/TrainDataGenerator.m
@@ -2,18 +2,32 @@ clear all
 close all
 clc
 
+%% Thumbs file control
+prompt = 'Thumbs.db existe? [y/n]';
+answer = input(prompt,'s');
+
+if isempty(answer)
+    answer = 'Y';
+end
+possibleAnswers = {'y','Y'};
+tf              = strcmp(answer,possibleAnswers);
+
 %% Path parameters
-box_size    = 25;
+box_size    = 31;
 curr_folder = pwd;
 
 targ_folder = [curr_folder '\Data\'];
 imag_folder = [curr_folder '\Data\Blobs\'];
 
 imag_folder_struct = dir(imag_folder);
+
 totImages          = length(imag_folder_struct(not([imag_folder_struct.isdir])));
+if tf(1)==1 || tf(2)==1
+    totImages          = length(imag_folder_struct(not([imag_folder_struct.isdir])))-1;
+end
+
 
 %% Data
-%Reading
 Data = zeros(totImages,box_size,box_size,3,'uint8');
 
 for image_nbr = 1:totImages
